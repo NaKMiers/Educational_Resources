493f4e06e7ce6500f7131dbc43fa2baeaee2fd64
Pi Pi <56575187+NaKMiers@users.noreply.github.com>
2024-05-08 17:00:33 +0700

    ok

--- src/app/api/admin/category/add/route.ts
+++ src/app/api/admin/category/add/route.ts
@@ -1,10 +1,7 @@
-import { connectDatabase } from '@/config/database'
-import CategoryModel from '@/models/CategoryModel'
 import { NextRequest, NextResponse } from 'next/server'
 
 // Models: Category
 import '@/models/CategoryModel'
-import { uploadFile } from '@/utils/uploadFile'
 
 // [POST]: /admin/tag/add
 export async function POST(req: NextRequest) {
@@ -12,37 +9,39 @@ export async function POST(req: NextRequest) {
 
   try {
     // connect to database
-    await connectDatabase()
+    // await connectDatabase()
 
-    // get data to create product
-    const formData = await req.formData()
-    const data = Object.fromEntries(formData)
-    const { title, color } = data
-    let logo = formData.get('logo')
+    // // get data to create product
+    // const formData = await req.formData()
+    // const data = Object.fromEntries(formData)
+    // const { title, color } = data
+    // let logo = formData.get('logo')
 
-    // check avatar
-    if (!logo) {
-      return NextResponse.json({ message: 'Không có ảnh đại diện nào' }, { status: 400 })
-    }
+    // // check avatar
+    // if (!logo) {
+    //   return NextResponse.json({ message: 'Không có ảnh đại diện nào' }, { status: 400 })
+    // }
 
-    // upload avatar and get imageUrl from AWS S3 Bucket
-    const imageUrl = await uploadFile(logo, '1:1')
+    // // upload avatar and get imageUrl from AWS S3 Bucket
+    // const imageUrl = await uploadFile(logo, '1:1')
 
-    // create new tag
-    const newCategory = new CategoryModel({
-      title: (title as string).trim(),
-      color: color,
-      logo: imageUrl,
-    })
+    // // create new tag
+    // const newCategory = new CategoryModel({
+    //   title: (title as string).trim(),
+    //   color: color,
+    //   logo: imageUrl,
+    // })
 
-    // save new tag to database
-    await newCategory.save()
+    // // save new tag to database
+    // await newCategory.save()
 
-    // stay current page
-    return NextResponse.json(
-      { message: `Category "${newCategory.title}" has been created` },
-      { status: 201 }
-    )
+    // // stay current page
+    // return NextResponse.json(
+    //   { message: `Category "${newCategory.title}" has been created` },
+    //   { status: 201 }
+    // )
+
+    return NextResponse.json({ message: 'Chức năng đang phát triển' }, { status: 501 })
   } catch (err: any) {
     return NextResponse.json({ message: err.message }, { status: 500 })
   }
